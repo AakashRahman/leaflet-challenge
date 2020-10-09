a86f4153e9a8d401b286950f3fea65044e616859
Aakash Rahman <zubaydur.aakash@gmail.com>
2020-10-08 22:41:36 -0400

    Add files via upload

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,26 +1,27 @@
-//Visualize an Earthquake data set
- // Create our map, giving it the streetmap and earthquakes layers to display on load
- // Creating our initial map object
-// We set the longitude, latitude, and the starting zoom level for sf
-var myMap = L.map("mapid", {
-  center: [37.7749, -122.4194],
-  zoom: 5
-});
+//Visualize an Earthquake map using earthquake dataset
 
-// Adding a tile layer (the background map image) to our map
-// We use the addTo method to add objects to our map
-L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-    tileSize: 512,
-    maxZoom: 18,
-    zoomOffset: -1,
-    id: "mapbox/streets-v11",
-    accessToken: API_KEY
-  }).addTo(myMap);
+ // Creating our initial map object
+
+var myMap = L.map("mapid", {
+  center: [
+    40.7, -94.5
+  ],
+  zoom: 4,
+  //layers: [streetmap]
+});
+ // Create our map, giving it the streetmap and earthquakes layers to display on load
+var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  tileSize: 512,
+  maxZoom: 18,
+  zoomOffset: -1,
+  id: "mapbox/streets-v11",
+  accessToken: API_KEY
+}).addTo(myMap);
 
 // Store our API endpoint
 
-var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson";
+var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson";
 
 //  GET color radius call to the query URL
 d3.json(queryUrl, function(data) {
@@ -29,15 +30,15 @@ d3.json(queryUrl, function(data) {
   return {
     opacity: 1,
     fillOpacity: 1,
-    fillColor: getColor(feature.properties.mag),
+    fillColor: getColor(feature.geometry.coordinates[2]),
     color: "#000000",
     radius: getRadius(feature.properties.mag),
     stroke: true,
     weight: 0.5
-  };
+  }
 }
 
-  // set different color from magnitude
+  // // set different color from magnitude
   function getColor(magnitude) {
     switch (true) {
     case magnitude > 5:
@@ -60,9 +61,9 @@ d3.json(queryUrl, function(data) {
       return 1;
     }
 
-    return magnitude * 4;
+    return magnitude * 5;
   }
-    // GeoJSON layer
+  //   // GeoJSON layer
     L.geoJson(data, {
       // Maken cricles
       pointToLayer: function(feature, latlng) {
@@ -72,30 +73,20 @@ d3.json(queryUrl, function(data) {
       style: styleInfo,
       // bind popup for each marker
       onEachFeature: function(feature, layer) {
-        layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
+        layer.bindPopup("<h1>  Magnitude:  " + feature.properties.mag + "</h1><br> <h1>Location: " + feature.properties.place +"</h1>");
       }
     }).addTo(myMap);
   
-    // an object legend
-    var legend = L.control({
-      position: "bottomright"
-    });
+  // add an object legend
+    var legend = L.control({position: "bottomright"});
   
-    // details for the legend
+    // details for legend
     legend.onAdd = function() {
       var div = L.DomUtil.create("div", "info legend");
-  
       var grades = [0, 1, 2, 3, 4, 5];
-      var colors = [
-        "#98ee00",
-        "#d4ee00",
-        "#eecc00",
-        "#ee9c00",
-        "#ea822c",
-        "#ea2c2c"
-      ];
+      var colors = ["#98ee00","#d4ee00","#eecc00","#ee9c00","#ea822c","#ea2c2c"];
   
-      // Looping through
+  //     // Looping through
       for (var i = 0; i < grades.length; i++) {
         div.innerHTML +=
           "<i style='background: " + colors[i] + "'></i> " +
@@ -104,6 +95,6 @@ d3.json(queryUrl, function(data) {
       return div;
     };
   
-    // Finally, we our legend to the map.
+    // Finally, our legend to the map.
     legend.addTo(myMap);
   });
